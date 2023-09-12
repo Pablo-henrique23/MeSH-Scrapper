53a08a6fdf22191e4a75f74b2b76eb727e1498fb
Pablo Henrique <105394456+zecabum@users.noreply.github.com>
2023-09-12 13:20:48 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ from bs4 import BeautifulSoup
 import requests
 from funcoes import *
 
-BACKSLASH = "\\"
-req = requests.get(input("URL> "))
+BACKSLASH = "\\" 
+req = requests.get(input("URL> ").strip())
 
+titulo = BeautifulSoup(req.content, 'html.parser')
 html = str(req.content)[str(req.content).find('Entry Terms'):]
 html = html[:html.find('</ul>')]
-soup = BeautifulSoup(html, 'html.parser')
 lista_termos = []
+
+for item in titulo.find_all("h1", class_="title"):
+    lista_termos.append(item.string)
+
+soup = BeautifulSoup(html, 'html.parser')
 for item in soup.find_all('li'):
     lista_termos.append(rf"{item.string.replace(BACKSLASH, '')}")
 
 termos = '('+" OR ".join(formatar(lista_termos))+')'
-print(termos)
+print("\n"+termos)
