dea424fe6bc214a774ecfc9595666e34ed06aee4
Pablo Henrique <105394456+zecabum@users.noreply.github.com>
2023-09-22 02:42:17 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -3,21 +3,26 @@ import requests
 from funcoes import *
 
 BACKSLASH = "\\" 
-linha()
-req = requests.get(input("URL> ").strip())
-linha()
-titulo = BeautifulSoup(req.content, 'html.parser')
-html = str(req.content)[str(req.content).find('Entry Terms'):]
-html = html[:html.find('</ul>')]
-lista_termos = []
+try:
+    linha()
+    req = requests.get(input("URL> ").strip())
+except OSError:
+    print("Impossivel fazer conexao, tente novamente")
+    exit
+else:    
+    linha()
+    titulo = BeautifulSoup(req.content, 'html.parser')
+    html = str(req.content)[str(req.content).find('Entry Terms'):]
+    html = html[:html.find('</ul>')]
+    lista_termos = []
 
-for item in titulo.find_all("h1", class_="title"):
-    lista_termos.append(item.string.replace(BACKSLASH, ''))
+    for item in titulo.find_all("h1", class_="title"):
+        lista_termos.append(item.string.replace(BACKSLASH, ''))
 
-soup = BeautifulSoup(html, 'html.parser')
-for item in soup.find_all('li'):
-    lista_termos.append(rf"{item.string.replace(BACKSLASH, '')}")
+    soup = BeautifulSoup(html, 'html.parser')
+    for item in soup.find_all('li'):
+        lista_termos.append(rf"{item.string.replace(BACKSLASH, '')}")
 
-termos = '('+" OR ".join(formatar(lista_termos))+')'
-print(f"\n[+] {termos}")
-print(f'\n[+] https://pubmed.ncbi.nlm.nih.gov/?term={termos.replace(" ", "+")}')
+    termos = '('+" OR ".join(formatar(lista_termos))+')'
+    print(f"\n[+] {termos}")
+    print(f'\n[+] https://pubmed.ncbi.nlm.nih.gov/?term={termos.replace(" ", "+")}')
